289592198b85e1ae5f5f8d0d8cc5b2d2638c3194
creativezen <tocha2782@gmail.com>
2022-04-17 20:40:48 +0300

    + 404.html, search.html

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,11 +1,9 @@
-import { checkboxSet } from './components/checkbox'
 import './components/menu'
-import './components/scroll'
 import './components/slider'
 import './components/modal'
+import './components/scroll'
 import './components/validation'
-import './components/map'
 import './components/toggleblock'
 import './components/search'
-
-// checkboxSet()
+import './components/lazy'
+import './components/map'
--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -1,2 +1,11 @@
 import SmoothScroll from 'smooth-scroll'
-const scroll = new SmoothScroll('a[href*="#"]')
+const scroll = new SmoothScroll('[data-scroll]', {
+  updateURL: true
+})
+
+const logTargetEvent = function(e) {
+  console.log('toggle:', e.detail.toggle.href)
+  console.log('anchor:', e.detail.anchor.id);
+}
+
+// document.addEventListener('scrollStart', logTargetEvent, false)
