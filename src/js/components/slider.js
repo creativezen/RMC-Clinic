f476864f1a9630ed11e47a77aa5bd7505274633b
Anton Goul'art <tocha2782@gmail.com>
2022-04-18 22:32:10 +0300

    re-write slider code, + animation for home slider

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -29,208 +29,47 @@ const swiperHero = new Swiper('.js-swiper-hero', {
   }
 });
 
-callSwiperSpecialist(document.querySelector('.js-swiper-specialist'))
-callSwiperServices(document.querySelector('.js-swiper-service'))
-callSwiperFeedbacks(document.querySelector('.js-swiper-feedback'))
-callSwiperNews(document.querySelector('.js-swiper-news'))
-callSwiperPartners(document.querySelector('.js-swiper-partner'))
-callSwiperSales(document.querySelector('.js-swiper-sales'))
+const sliders = [
+  {name: 'specialist'},
+  {name: 'service'},
+  {name: 'feedback'},
+  {name: 'news'},
+  {name: 'partner'},
+  {name: 'sales'}
+]
 
+while (sliders.length) {
 
-function callSwiperSpecialist(selector) {
-  if (selector) {
-    const sliderSpecialist = new Swiper(selector, {
-      slidesPerView: "auto",
-      spaceBetween: 10,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true
+  let sliderItem = sliders.shift()
+  let sliderSelector = `.js-swiper-${sliderItem.name}`
+
+  new Swiper(sliderSelector, {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true
+    },
+    observer: true,
+    observeParents: true,
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
       },
-      observer: true,
-      observeParents: true,
-      breakpoints: {
-        480: {
-          slidesPerView: 2,
-        },
-        1080: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        1280: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-          navigation: {
-            nextEl: ".swiper-button-next.next-specialist",
-            prevEl: ".swiper-button-prev.prev-specialist",
-          },
-        },
-      }
-    })
-  }
-}
-
-function callSwiperServices(selector) {
-  if (selector) {
-    const sliderServices = new Swiper(selector, {
-      slidesPerView: "auto",
-      spaceBetween: 10,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true
+      1080: {
+        slidesPerView: 3,
+        spaceBetween: 30
       },
-      observer: true,
-      observeParents: true,
-      breakpoints: {
-        480: {
-          slidesPerView: 2,
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+        navigation: {
+          nextEl: `.swiper-button-next.next-${sliderItem.name}`,
+          prevEl: `.swiper-button-prev.prev-${sliderItem.name}`,
         },
-        1080: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        1280: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-          navigation: {
-            nextEl: ".swiper-button-next.next-service",
-            prevEl: ".swiper-button-prev.prev-service",
-          },
-        },
-      }
-    })
-  }
-}
-
-function callSwiperFeedbacks(selector) {
-  if (selector) {
-    const sliderFeedbacks = new Swiper(selector, {
-      slidesPerView: "auto",
-      spaceBetween: 10,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true
       },
-      observer: true,
-      observeParents: true,
-      breakpoints: {
-        480: {
-          slidesPerView: 2,
-        },
-        1080: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        1280: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-          navigation: {
-            nextEl: ".swiper-button-next.next-feedback",
-            prevEl: ".swiper-button-prev.prev-feedback",
-          },
-        },
-      }
-    })
-  }
-}
-
-function callSwiperNews(selector) {
-  if (selector) {
-    const sliderFeedbacks = new Swiper(selector, {
-      slidesPerView: "auto",
-      spaceBetween: 10,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true
-      },
-      observer: true,
-      observeParents: true,
-      breakpoints: {
-        480: {
-          slidesPerView: 2,
-        },
-        1080: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        1280: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-          navigation: {
-            nextEl: ".swiper-button-next.next-news",
-            prevEl: ".swiper-button-prev.prev-news",
-          },
-        },
-      }
-    })
-  }
-}
-
-function callSwiperPartners(selector) {
-  if (selector) {
-    const sliderFeedbacks = new Swiper(selector, {
-      slidesPerView: "auto",
-      spaceBetween: 10,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true
-      },
-      observer: true,
-      observeParents: true,
-      breakpoints: {
-        480: {
-          slidesPerView: 2,
-        },
-        1080: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        1280: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-          navigation: {
-            nextEl: ".swiper-button-next.next-partner",
-            prevEl: ".swiper-button-prev.prev-partner",
-          },
-        },
-      }
-    })
-  }
-}
-
-function callSwiperSales(selector) {
-  if (selector) {
-    const sliderFeedbacks = new Swiper(selector, {
-      slidesPerView: "auto",
-      spaceBetween: 10,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true
-      },
-      observer: true,
-      observeParents: true,
-      breakpoints: {
-        780: {
-          slidesPerView: 2,
-        },
-        1080: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        1280: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-          navigation: {
-            nextEl: ".swiper-button-next.next-sales",
-            prevEl: ".swiper-button-prev.prev-sales",
-          },
-        },
-      }
-    })
-  }
+    }
+  })
+  continue
 }
