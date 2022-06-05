1665a0ccd4167a6540b04aa5ed25b54d9120c953
Anton Goul'art <tocha2782@gmail.com>
2022-06-05 15:22:55 +0300

    + new styles end js for disable zoom in browser

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 import './_vendor';
 import './_components';
 import './services/requests'
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Блокируем функцию зумирование в браузере
+  var keyCodes = [61, 107, 173, 109, 187, 189];
+
+  document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey == true && (keyCodes.indexOf(event.which) != -1)) {
+      console.log('Масштабирование окна отключено');
+      event.preventDefault();
+    }
+    if (event.metaKey == true && (keyCodes.indexOf(event.which) != -1)) {
+      console.log('Масштабирование окна отключено');
+      event.preventDefault();
+    }
+  })
+
+  window.addEventListener('mousewheel', function (event) {
+    if (event.ctrlKey == true || event.metaKey == true) {
+      alert('Масштабирование окна отключено');
+      event.preventDefault();
+    }
+  }, { passive: false });
+})
